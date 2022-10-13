3a7ed21517a29fff819307ff1d3f703ca99fbe70
imbajin <jin@apache.org>
2022-10-14 00:42:00 +0800

    feat(apache): support check license header with RAT
    use `apache-rat:check` to see the report
    
    and fix current license problems
    
    TODO: we should add & use apache-rat plugin in other repos later
    
    BTW, we'd reuse such configs better (rather than copy them in each repo)

--- hugegraph-common/src/test/java/org/apache/hugegraph/unit/concurrent/BarrierEventTest.java
+++ hugegraph-common/src/test/java/org/apache/hugegraph/unit/concurrent/BarrierEventTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2017 HugeGraph Authors
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.hugegraph.unit.concurrent;
 
 import java.util.concurrent.CountDownLatch;
@@ -13,7 +32,7 @@ import org.apache.hugegraph.testutil.Assert;
 
 public class BarrierEventTest {
     
-    private static int WAIT_THREADS_COUNT = 10;
+    private static final int WAIT_THREADS_COUNT = 10;
 
     @Test(timeout = 5000)
     public void testAWait() throws InterruptedException {
