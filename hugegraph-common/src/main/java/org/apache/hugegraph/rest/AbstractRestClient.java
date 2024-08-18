3903d24a67ac7c044de695a709a68a0a48ea0c59
Thespica <thespica@qq.com>
2024-08-18 15:57:31 +0800

    shrink replace rules

--- hugegraph-common/src/main/java/org/apache/hugegraph/rest/AbstractRestClient.java
+++ hugegraph-common/src/main/java/org/apache/hugegraph/rest/AbstractRestClient.java
@@ -416,14 +416,7 @@ public abstract class AbstractRestClient implements RestClient {
 
     public static String encode(String raw) {
         try {
-            String encoded = URLEncoder.encode(raw, StandardCharsets.UTF_8.toString());
-            return encoded.replace("+", "%2B")
-                    .replace(" ", "%20")
-                    .replace("/", "%2F")
-                    .replace("?", "%3F")
-                    .replace("#", "%23")
-                    .replace("&", "%26")
-                    .replace("=", "%3D");
+            return URLEncoder.encode(raw, StandardCharsets.UTF_8.toString()).replace("+", "%2B");
         } catch (UnsupportedEncodingException e) {
             throw new IllegalArgumentException("Failed to encode string: " + raw, e);
         }
